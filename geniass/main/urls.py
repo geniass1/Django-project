df1a3f5c18deb27a8b470e5276975dbb403f40e2
geniass1 <amingoma2003@gmail.com>
2021-01-11 15:21:14 +0300

    database functional was updated

--- geniass/main/urls.py
+++ geniass/main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='main'),
-    path('lox', views.da, name='second'),
+    path('create', views.create, name='create'),
 ]
--- geniass/main/views.py
+++ geniass/main/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Lol', 'tasks': tasks})
 
 
-def da(request):
-    return HttpResponse("<h4>daYAADAD<h4>")
+def create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        form.save()
+        return redirect('/')
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)
+
