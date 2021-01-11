6fd97683907d79b773e28e9b031e44de25a35c11
geniass1 <amingoma2003@gmail.com>
2021-01-11 15:22:54 +0300

    create page was created

---
+++ geniass/main/forms.py
@@ -0,0 +1,16 @@
+from .models import Task
+from django.forms import ModelForm, TextInput
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+            'class': 'form-control'
+        }),
+            "task": TextInput(attrs={
+            'class': 'form-control'
+        }),
+    }
