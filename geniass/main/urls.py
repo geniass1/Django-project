1eb3b5ef08eb0b542e7b065cb4f82f2e700406ae
geniass1 <amingoma2003@gmail.com>
2021-01-14 19:57:11 +0300

    search bar was added and start of user page

--- geniass/main/urls.py
+++ geniass/main/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('task/<int:id>/', views.task_id, name='task_id'),
     path('delete/<int:id>/', views.delete, name='delete'),
+
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- geniass/main/views.py
+++ geniass/main/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, redirect
 from .models import Product
 from .forms import ProductForm
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
-    if len(request.GET)==0  or request.GET['a'] == '2':
-        tasks = Product.objects.order_by('-id')
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        index_by_date = f'/?a=2&search={search}'
+        index_by_price = f'/?a=1&search={search}'
+        tasks = Product.objects.all().filter(title=search)
     else:
+        search = ""
+        index_by_date = '/?a=2'
+        index_by_price = '/?a=1'
         tasks = Product.objects.order_by('-price')
-    return render(request, 'main/index.html', {'title': 'Lol', 'tasks': tasks})
+    if 'a' not in request.GET or len(request.GET) == 0 or request.GET['a'] == '2':
+        tasks = tasks.order_by('-price')
+    elif request.GET['a'] == '1':
+        tasks = tasks.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Lol', 'tasks': tasks, 'index_by_date': index_by_date,'index_by_price': index_by_price,'search':search})
+
+
+def login(request):
+    context = {}
+    return render(request, 'main/login.html', context)
+
+
+@csrf_exempt
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'main/register.html', context)
 
 
 def task_id(request, id):
